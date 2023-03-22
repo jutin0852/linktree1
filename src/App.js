253033d4600c81afcab817194ceef1becf9d5d59
jutin0852 <jutindikonu8@gmail.com>
2023-03-22 21:12:29 +0100

    done i hope

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Profile from "./component/profile";
-import LinkTree from "./component/linktree";
+import LinkTree from "./component/content";
 import Icon from "./component/icon";
 import Footer from "./component/footer";
 function App() {
